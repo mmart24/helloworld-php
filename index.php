1f37e6db1beae925ed22ea6ecc699a22db13dbc5
mmart24 <60648277+mmart24@users.noreply.github.com>
2020-03-13 18:04:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World 2021</p>'; ?> 
- </body>
-</html>
+<?php
+
+echo "Welcome to OpenShift Online Developer Preview";
+echo "<br>To test the database, hit the dbtest.php URL";
+
+
+echo "<br><br>This assumes that you have the correct env variables set";
+echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
+
